068fea1b0832683fc957c1c8103bc416e72b944d
Cory Davis <cd_davis93@yahoo.com>
2018-12-03 16:40:48 -0600

    maybe the last one

--- dft1.py
+++ dft1.py
@@ -28,8 +28,10 @@ loc_imag = np.zeros(n/size)
 mas_sumreal = 0.0
 mas_sumimag = 0.0
 
-comm.Scatter(real,loc_real,root=0)
-comm.Scatter(imag,loc_imag,root=0)
+#comm.Scatter(real,loc_real,root=0)
+#comm.Scatter(imag,loc_imag,root=0)
+comm.Bcast(real,root=0)
+comm.Bcast(imag,root=0)
 
 print (rank, loc_real, loc_imag)
 
@@ -41,18 +43,19 @@ for k in range(rank*n/size,n/size*(rank+1)): #n/size,size):  # For each output e
 	#print("loop")
 	#print(k)
 
-	for t in range(0,n/size): #rank*n/size,n/size*(rank+1)): #2*rank,2*(rank+1)): #0,n/size,size):  # For each input element
+	for t in range(0,n): #/size): #rank*n/size,n/size*(rank+1)): #2*rank,2*(rank+1)): #0,n/size,size):  # For each input element
 		angle = 2 * math.pi * (t+ rank*n/size) * k / n #t * k / n # 
-		loc_sumreal +=  loc_real[t] * math.cos(angle) + loc_imag[t] * math.sin(angle)
-		loc_sumimag += -loc_real[t] * math.sin(angle) + loc_imag[t] * math.cos(angle)
+		loc_sumreal +=  real[t] * math.cos(angle) + imag[t] * math.sin(angle)
+		loc_sumimag += -real[t] * math.sin(angle) + imag[t] * math.cos(angle)
 
-	if (k == 2*rank):
+	if (k == rank*n/size):
 		loc_outreal = np.array([loc_sumreal])
 		loc_outimag = np.array([loc_sumimag])
 	else:	
 		loc_outreal = np.append(loc_outreal,loc_sumreal)
 		loc_outimag = np.append(loc_outimag,loc_sumimag)
-
+	
+	print (rank, k, loc_outreal, loc_outimag)
 	#print (loc_outreal)
 	#print (loc_outimag)
 
@@ -61,6 +64,7 @@ comm.Gather(loc_outreal,outreal,root=0)
 comm.Gather(loc_outimag,outimag,root=0)
 
 if (rank == 0):
+	print ("Final arrays")	
 	print outreal
 	print outimag
 
